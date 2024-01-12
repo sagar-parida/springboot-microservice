dc4cf50fed5bbb9be908d102a086e8e22e60f01b
sagar-parida <paridag321@gmail.com>
2024-01-12 16:48:57 +0530

    Inventory service Added

---
+++ inventory-service/src/main/java/com/demoproject1/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,32 @@
+package com.demoproject1.inventoryservice;
+
+import com.demoproject1.inventoryservice.model.Inventory;
+import com.demoproject1.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("maggi_1_2");
+			inventory.setQuantity(50);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("yippi_3_4");
+			inventory1.setQuantity(100);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
+}
