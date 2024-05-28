19e2d4b136bcc50d578ab064e26a4ca34108ed4c
sagar-parida <paridag321@gmail.com>
2024-05-28 23:02:01 +0530

    Global Exception Handler for Cards service

--- cards/src/main/java/com/spring_microservice/cards/CardsApplication.java
+++ cards/src/main/java/com/spring_microservice/cards/CardsApplication.java
@@ -1,9 +1,31 @@
 package com.spring_microservice.cards;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards Microservice",
+				description = "Cards Microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Sagar Parida",
+						email= "paridag321@gmail.com",
+						url = "www.google.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.google.com"
+				)
+		)
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
